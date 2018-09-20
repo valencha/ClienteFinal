a70b2c1c6511034d566ea71d604acf99f407a663
Valentina Chavez <valencha10@gmail.com>
2018-09-20 13:14:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estudiante/cliente/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.estudiante.cliente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements Receptor.OnMessage{
+    private Button btn_enviar;
+    private Cliente c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_enviar= findViewById(R.id.btn_enviar);
+
+        c = new Cliente(this);
+       c.start();
+
+        btn_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            c.enviar();
+            }
+        });
+
+    }
+
+    @Override
+    public void onReceived(final String mensaje) {
+    runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+            Toast.makeText(MainActivity.this,mensaje,Toast.LENGTH_SHORT).show();
+        }
+    });
+    }
+}
